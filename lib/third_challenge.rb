7ddd67477bbc2b42460da311bed05e5e526733f8
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-23 00:23:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,8 +18,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        :name=>{},
-        :age=>{}
+        #your key/value pairs here
         },
       :matriarch => {
         #your key/value pairs here
